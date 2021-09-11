b2a7aff837014d42dfe1e18cfc36426f437ca5f4
Midgxt <56519750+Midgxt@users.noreply.github.com>
2021-09-11 16:33:51 +0100

    Update entrypoint.sh

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -5,10 +5,18 @@ cd /home/container
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Print Node.js Version
-node -v
+VER=$(node -v)
+
+printf "|------------------------------------------------|"
+printf "| "
+printf "| Thank you for choosing Elytron Discord Hosting!"
+printf "| You are currently using ${VER}, to change this please open a ticket."
+printf "| To install packages, please be sure they are set in your package.json file!"
+printf "| "
+printf "|------------------------------------------------|"
 
 # Replace Startup Variables
-MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+MODIFIED_STARTUP=$(echo -e if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; /usr/local/bin/node /home/container/${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
